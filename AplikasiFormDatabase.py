da0dd62369f9714c4eb8766b8c4faab9afa31cc0
Dzikriananda <96009309+Dzikriananda@users.noreply.github.com>
2023-05-26 23:12:24 +0700

    Revert "Revert "add note""
    This reverts commit 45840fb7563132e0aeb0016d410556759e9e772b.

--- AplikasiFormDatabase.py
+++ AplikasiFormDatabase.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import sqlite3
 from tkinter import messagebox as tkmbox
 
-window=tk.Tk()
+window=tk.Tk() #ini adalah syntax pertama tkinter
 
 frame_input=ttk.Frame(window)
 frame_input.pack(padx=10,pady=10)
